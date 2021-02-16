1d9cf99d82b0f07c32b0eb4307bdd31f0a4d461b
Jesper Devantier <jesper.devantier@protonmail.com>
2021-02-16 18:13:51 +0100

    save for later

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -7,10 +7,12 @@
             [ajax.core :as http]
             [reitit.frontend :as reitit]
             [accountant.core :as accountant]
-            ))
+            ["hotkeys-js" :as hotkeys]))
 
 (defn url [& paths]
   (string/join "/" paths))
+
+(defonce state (r/atom {}))
 
 ; ---------------- router
 (def router
@@ -27,6 +29,8 @@
 
 
 ; ---------------- components
+
+
 (defn atom-input [value]
   [:input {:type "text"
            :value @value
@@ -39,13 +43,42 @@
        [:p " the value is now: " @val]
        [:p "Change it here: " [atom-input val]]])))
 
+(defn tabbed-component [state tabs]
+  (reset! state {:active (-> tabs keys first)})
+  (fn []
+    (js/console.log "re-render -- active:" (:active @state))
+    (let [active (:active @state)]
+      [:div.tab
+       [:div.tab-header
+        {:style {:border-bottom "1px solid #000"
+                 :padding-bottom ".3em"}}
+        (for [[ndx title] (map-indexed vector (keys tabs))]
+          [:span.tab-item
+           {:style (merge {:border-bottom (if (= title active)
+                                            "2px solid #1573B9" "")
+                           :cursor "pointer"}
+                          (when (not (= ndx 0))
+                            {:margin-left ".4em"}))
+            :on-click #(do (js/console.log "clicked")
+                           (swap! state (fn [s] (assoc s :active title))))
+            :key title} title])]
+       [(get tabs active)]])))
+
+(defn c-one []
+  [:h3 "one"])
+(defn c-two []
+  [:h3 "two"])
+
+(def tab-state (r/atom {}))
+
 (defn hello-world []
   [:div
    [:h1 "hello, from react"]
    [:div
     [shared-state]
     [:button#fetch {:on-click (fn []
-                                (http/GET (url "/api" "tags")))} "fetch"]]])
+                                (http/GET (url "/api" "tags")))} "fetch"]
+    [(tabbed-component tab-state {"one" c-one "two" c-two})]]])
 
 (defn tags-index []
   [:h1 "tags index"])
@@ -60,21 +93,98 @@
     :tags-index #'tags-index
     :tag-results #'tag-results))
 
+(defn popup-show!
+  ([component] (popup-show! component {}))
+  ([component opts]
+   (swap! state (fn [s] (update s :popup #(merge
+                                           %
+                                           (select-keys opts [:can-close?])
+                                           {:component component
+                                            :show? true}))))))
+
+(defn popup-hide!
+  []
+  (swap! state (fn [s] (update s :popup #(assoc % :show? false)))))
+
+(defn popup-show?
+  []
+  (get-in @state [:popup :show?] false))
+
+(defn popup []
+  (fn []
+    (let [{:keys [component show? can-close?]
+           :or {show? false
+                can-close? #(constantly true)}} (get @state :popup)]
+      [:div {:style {:position "fixed"
+                     :top 0 :bottom 0 :right 0 :left 0
+                     :background "rgba(0, 0, 0, 0.7)"
+                     :transition "all 200ms ease-in-out"
+                     :visibility (if show? "visible" "hidden")
+                     :opacity (if show? 100 0)}}
+       [:div {:style {:margin "70px auto"
+                      :padding "20px"
+                      :background "#fff"
+                      :border-radius "5px"
+                      :width "70%"
+                      :position "relative"
+                      :transition "all 2s ease-in-out"}}
+        [:a {:style {:position "absolute"
+                     :top ".2em" :right ".4em"
+                     :transition "all 200ms"
+                     :font-size "30px"
+                     :font-weight "bold"
+                     :text-decoration "none"
+                     :color "#333"}
+             :href "#"
+             :on-click #(when (can-close?)
+                          (swap! state (fn [s] (assoc-in s [:popup :show?] false))))} "×"]
+        (when component [component])]])))
+
 ; ---------------- page mounting component
+
+
 (defn current-page []
   (fn []
     (let [page (:current-page (session/get :route))]
       (js/console.warn (session/get :route))
       [:div
-       [:header "hello"]
-       [page]
-       ])))
+       [(popup)]
+       [:header
+        {:style {:position "fixed" :top 0 :left 0 :right 0
+                 :background-color "#1573B9"
+                 :color "white"}}
+        [:div
+         [:span "🔍"]
+         [:span "Tags"]]
+        "acknowledge me"]
+       [page]])))
 
 ; ---------------- hot reloading
 
+; https://www.npmjs.com/package/hotkeys-js
+(hotkeys "f4" (fn [event handler]
+                (.preventDefault event)
+                (if (= (. event -type) "keydown")
+                  (js/console.log "f4 keydown")
+                  (js/console.log "f4 keyup"))))
+
+(hotkeys "ctrl+p" (fn [event handler]
+                    (.preventDefault event)
+                    (js/console.log "ctrl-p")
+                    (popup-show! c-two)))
+
+(hotkeys "escape" (fn [event handler]
+                    (.preventDefault event)
+                    (when (popup-show?)
+                      (popup-hide!))
+                    (js/console.log "esc")))
+
 (defn -start []
   ; called after reload
-  (rdom/render [current-page] (gdom/getElement "app")))
+  (rdom/render [current-page] (gdom/getElement "app"))
+  (js/console.log @state)
+  #_(swap! state (fn [s] (assoc s :popup {:component c-two
+                                          :show? true}))))
 
 (defn ^:export init []
   ; called only once on app load
@@ -92,9 +202,7 @@
     (fn [path]
       (boolean (reitit/match-by-path router path)))})
   (accountant/dispatch-current!)
-  (-start)
-  #_(js/console.log {:hello "world!!"
-                   :is-map true}))
+  (-start))
 
 (defn -stop []
   ; called before any code is reloaded, see shadow-cljs :before-load
