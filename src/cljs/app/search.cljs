b2effa289412bbebf708d3c356c1787135492bcc
Jesper Devantier <jesper.devantier@protonmail.com>
2021-03-13 21:14:21 +0100

    refac: separate visual from logic components

--- src/cljs/app/search.cljs
+++ src/cljs/app/search.cljs
@@ -6,6 +6,7 @@
             [goog.events.KeyCodes :as key]
             [accountant.core :as accountant]
             [app.state :as state]
+            [app.components.base :refer [input-field search-dialog]]
             [app.components.modal :as modal]
             [app.components.note :as note]
             [app.components.keys :refer [with-keys]]))
@@ -75,29 +76,22 @@
               (accountant/navigate! (str "/notes/" (get selected-result "id")))
               (modal/close!)))}
       (fn [on-close]
-        [:div {:class "flex flex-col w-full"}
-     ; search field
-         [:div {:class ["flex flex-none"]}
-          [:input {:type "text" :name "search-query"
-                   :id "search-query" :placeholder "query..."
-                   :class "w-full px-4 py-2 border-x-grey focus:border-x-grey-light border-solid border-2 bg-x-grey-dark w-full text-x-white focus:outline-none"
-                   :auto-complete "off"
-                   :value (get @cursor :query "")
-                   :ref (fn [el] (when el (. el focus)))
-                   :on-change
-                   (fn [event]
-                     (let [query-value (.. event -target -value)]
-                       (swap! cursor #(assoc % :query query-value))
-                       (http/POST (str "/api/search/notes")
-                         {:format :json
-                          :params {"search-query" query-value}
-                          :handler (fn [rsp] (swap! cursor (fn [s] (merge s {:results (get rsp "data" [])
-                                                                             :selected-ndx 0}))))
-                          :error-handler #(js/console.error %)})))}]]
-     ; search results
-         [:div {:class "flex flex-col flex-grow min-h-0 mt-4 overflow-y-scroll scrollbar-thin scrollbar-thumb-x-blue scrollbar-track-x-grey-dark"}
-          (let [{:keys [results selected-ndx] :or {results [] selected-ndx 0}} @cursor]
-            (map-indexed (fn [ndx item] (search-result (assoc item "active?" (= ndx selected-ndx)))) results))]]))))
+        [search-dialog
+         [input-field
+          {:value (get @cursor :query "")
+           :ref (fn [el] (when el (. el focus)))
+           :on-change
+           (fn [event]
+             (let [query-value (.. event -target -value)]
+               (swap! cursor #(assoc % :query query-value))
+               (http/POST (str "/api/search/notes")
+                 {:format :json
+                  :params {"search-query" query-value}
+                  :handler (fn [rsp] (swap! cursor (fn [s] (merge s {:results (get rsp "data" [])
+                                                                     :selected-ndx 0}))))
+                  :error-handler #(js/console.error %)})))}]
+         (let [{:keys [results selected-ndx] :or {results [] selected-ndx 0}} @cursor]
+           (map-indexed (fn [ndx item] (search-result (assoc item "active?" (= ndx selected-ndx)))) results))]))))
 
 (defn help
   "help page"
